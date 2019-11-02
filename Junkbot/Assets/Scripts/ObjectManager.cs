499f2711a1966268c521af450fd9e83b31323bff
sangar3 <55110746+sangar3@users.noreply.github.com>
2019-11-01 19:49:30 -0500

    first commit

---
+++ Junkbot/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public Vector3 resetPos;
+    public Quaternion resetRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetPos = transform.position;
+        resetRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -20 || gameObject.transform.position.y > -5)
+        {
+            transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.position = resetPos;
+
+            transform.rotation = resetRot;
+        }
+    }
+}
